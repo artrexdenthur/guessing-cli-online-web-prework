a9931e8f900bb43df6b4ba59250ead2a64e0aa23
Paul Ashour <paul.ashour@gmail.com>
2018-10-10 14:10:04 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,7 +5,13 @@ def run_guessing_game
     puts 'Guess a number between 1 and 6.'
     # take input from command line 
     command = gets.chomp
+    # check for exit
+    if command == 'exit'
     # campare input to random number
     r_num = rand(1..6)
       # if correct, puts 'You guessed the correct number!'
+      
+      # otherwise, puts 'The computer guessed 5.'
+    
+  end
 end
